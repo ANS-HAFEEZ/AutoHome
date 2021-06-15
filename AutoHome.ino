d44152b9cbb1219ec98fa30b34b7bb2d86209d7e
Ans Hafeez <ANS-HAFEEZ@users.noreply.github.com>
2021-06-15 23:29:26 +0500

    Get Status

--- AutoHome.ino
+++ AutoHome.ino
@@ -1,48 +1,99 @@
 #include <ESP8266WiFi.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
-
+#include <PubSubClient.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
+const char* ssid        = "PTCL_BB";
+const char* password    = "F4D428A4";
+const char* mqtt_server = "test.mosquitto.org";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
 #define DHTPIN 2     // Digital pin connected to the DHT sensor 
 #define DHTTYPE    DHT11  
 DHT_Unified dht(DHTPIN, DHTTYPE);
-#define WLAN_SSID       "zong"
-#define WLAN_PASS       "54022175"
 #define ACPin 14
-/************************* Adafruit.io Setup *********************************/
 
-#define AIO_SERVER      "test.mosquitto.org"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-
-/************ Global State (you don't need to change this!) ******************/
-// Create an ESP8266 WiFiClient class to connect to the MQTT server.
-WiFiClient client;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, "","");
-
-/****************************** Feeds ***************************************/
-
-// Setup a feed called 'photocell' for publishing.
-// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish Temp = Adafruit_MQTT_Publish(&mqtt,"/ans/home/Temp");
-Adafruit_MQTT_Publish Humi = Adafruit_MQTT_Publish(&mqtt,"/ans/home/Humi");
-
-// Setup a feed called 'onoff' for subscribing to changes.
-Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt,"/ans/home/AC");
-
-/*************************** Sketch Code ************************************/
-
-// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
-// for some reason (only affects ESP8266, likely an arduino-builder bug).
 void MQTT_connect();
+uint32_t x=0;
+static int cc = 0;
+static bool AUTO = true;
+static bool AC = true;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  if(strstr(topic,"AC"))
+  {
+      AC = (int) payload[0] %48;
+  }
+  if(strstr(topic,"AUTO"))
+  {
+      AUTO = (int) payload[0] %48;
+  }
+  Serial.println(AUTO);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESPAns121";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // ... and resubscribe
+      client.subscribe("ans/room/AUTO");
+      client.subscribe("ans/room/AC");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  delay(10);
+  setup_wifi();
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   pinMode(ACPin,OUTPUT);
@@ -53,99 +104,77 @@ void setup() {
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
   // Connect to WiFi access point.
-  Serial.println(); Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(WLAN_SSID);
-
-  WiFi.begin(WLAN_SSID, WLAN_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-
-  Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
-
-  // Setup MQTT subscription for onoff feed.
-  mqtt.subscribe(&onoffbutton);
+ 
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback); 
 }
 
-uint32_t x=0;
+
 void loop() {
-  MQTT_connect();
+  if (!client.connected()) {
+    reconnect();
+  }
   sensors_event_t event;
-  Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(5000))) {
-    if (subscription == &onoffbutton) {
-      Serial.print(F("Got: "));
-      char *Val = (char *)onoffbutton.lastread;
-      int cc = (int)*Val * 100;
-      Serial.println(cc);
-      if(cc == 4900)
-      {
-        Serial.println("True");
-        digitalWrite(ACPin,0);
-      }
-      else
-      {
-        Serial.println("False");
-        digitalWrite(ACPin,1);
-      }
-    }
-  }
-
   dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else {
+  float RoomTemp  = event.temperature;
+  if (!isnan(RoomTemp)) {
     Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
+    Serial.print(RoomTemp);
     Serial.println(F("°C"));
-    if (! Temp.publish(event.temperature)) {
-      Serial.println(F("Failed"));
-    }
   }
-  // Get humidity event and print its value.
   dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else {
+  int   RoomHumi  = event.relative_humidity;
+
+  char stemp[5] = {0};
+  char shumi[5] = {0};
+
+  char sAC[3]    = {0};
+  char sAUTO[3]  = {0};
+  
+
+
+  if (!isnan(RoomHumi)) {
     Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
+    Serial.print(RoomHumi);
     Serial.println(F("%"));
+  }
 
-    if (! Humi.publish(event.relative_humidity)) {
-      Serial.println(F("Failed"));
+
+  if(AUTO)
+  {
+    if(event.relative_humidity < 50 && event.temperature < 25)
+    {
+          Serial.println("AUTOOO 0000");
+      digitalWrite(ACPin,0);
     }
+    
+    if(event.relative_humidity > 50 && event.temperature > 28)
+    {
+      Serial.println("AUTOOO 1111");
+      digitalWrite(ACPin,1);
+    }  
   }
-}
-
-// Function to connect and reconnect as necessary to the MQTT server.
-// Should be called in the loop function and it will take care if connecting.
-void MQTT_connect() {
-  int8_t ret;
-
-  // Stop if already connected.
-  if (mqtt.connected()) {
-    return;
+  else
+  {
+    Serial.println("AUTOOO ELSE");
+    if(!AC)
+    {
+      digitalWrite(ACPin,1);
+    }
+    else
+    {
+      digitalWrite(ACPin,0);
+    }  
   }
-
-  Serial.print("Connecting to MQTT... ");
-
-  uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
-       }
-  }
-  Serial.println("MQTT Connected!");  
+  sprintf(stemp,"%f",RoomTemp);
+  sprintf(shumi,"%d",RoomHumi);
+  sprintf(sAUTO,"%d",AUTO);
+  sprintf(sAC,"%d",digitalRead(ACPin));
+  Serial.println(sAC);
+  client.publish("ans/room/temp",stemp);
+  client.publish("ans/room/humi",shumi);
+  client.publish("ans/room/AUTOs",sAUTO);
+  client.publish("ans/room/ACs"  ,sAC);
+  client.loop();
+  delay(1000);
 }
